9fc87d6e0b026840f4dbd6677ef97a77ba07f012
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-07-09 15:12:41 -0700

    rough draft of default graph

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -3,8 +3,8 @@ var svg_width = 1200;
 var svg_height = 600;
 
 var margin = {
-    top:50,
-    bottom:50,
+    top:80,
+    bottom:80,
     left:80,
     right:80
 }
@@ -19,10 +19,30 @@ function build_x_scale(census_data,chosen_x){
     var x_scale = d3.scaleLinear()
         .range([0,width])
         .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
-    return(x_scale) 
+    return x_scale
 }
 
+function build_y_scale(census_data,chosen_y){
+    var y_scale = d3.scaleLinear()
+        .range([height,0])
+        .domain([d3.min(census_data,d => d[chosen_y]*.7),d3.max(census_data,d => d[chosen_y])*1.2])
+    return y_scale
+}
+function build_bubbles(census_data,chosen_x,chartGroup,x_scale,y_scale){
+    chartGroup.selectAll("circle")
+    .data(census_data)
+    .enter()
+    .append("g")
+    .append("circle")
+    .attr("cx", d => x_scale(d.income))
+    .attr("cy",d => y_scale(d.healthcare))
+    .attr("r","20")
+    .attr("fill","steelblue")
+    .attr("opacity",".5")
+    .attr("stroke","steelblue")
+}
 var x_labels = ["income","obesity","age"]
+var y_labels = ["healthcare","poverty","smokes"]
 
 d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
     if (err) throw err;
@@ -48,20 +68,34 @@ d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
         .attr("transform",`translate(0,${height})`)
         .call(xAxis)
         .attr("id","x_axis")
-// x_labels.forEach(label=> 
-//     chartGroup.append("text")
-//         .attr("x",width/2.2)
-//         .attr("y",height + margin.top)
-//         .attr("id", "x_label")
-//         .text(`${label}`))
-x_labels.forEach(function(label,i){
-    chartGroup.append("text")
-    .attr("x",width/2.2)
-    .attr("y",height + margin.top - i*15)
-    .attr("id", "x_label")
-    .text(`${label} (%)`)
-    .attr("value",`${label}`)
-})
+
+    var yScale = build_y_scale(census_data,chosen_y)
+    var yAxis = d3.axisLeft(yScale)
+    chartGroup.append("g")
+        .call(yAxis)
+
+    x_labels.forEach(function(label,i){
+        chartGroup.append("text")
+        .attr("x",width/2.2)
+        .attr("y",height + margin.top - i*15)
+        .attr("id", "x_label")
+        .text(`${label} (%)`)
+        .attr("value",`${label}`)
+    })
+
+    y_labels.forEach(function(label,i){
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 40 - margin.left - 15*i)
+            .attr("x", 10 - (height / 2))
+            .attr("dy", "1em")
+            .classed("axis-text", true)
+            .text(`${label} (%)`)
+            .attr("value",`${label}`)
+            .attr("id","test_bunny")
+    })
+    build_bubbles(census_data,chosen_x,chartGroup,xScale,yScale)
+
 
 // rough draft creating text labels
     // var labels = chartGroup.append("text")
@@ -84,11 +118,11 @@ x_labels.forEach(function(label,i){
 
 
     // var x_scale = d3.scaleLinear()
-    //     .range([0,width])
-    //     .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
+        // .range([0,width])
+        // .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
     // var y_scale = d3.scaleLinear()
-    //     .range([height,0])
-    //     .domain([d3.min(census_data,d => d[chosen_y]*.7),d3.max(census_data,d => d[chosen_y])*1.2])
+        // .range([height,0])
+        // .domain([d3.min(census_data,d => d[chosen_y]*.7),d3.max(census_data,d => d[chosen_y])*1.2])
 
     // var bottom_axis = d3.axisBottom(x_scale)
     
@@ -135,14 +169,14 @@ x_labels.forEach(function(label,i){
     //     .text("Healthcare")
     //     .attr("value","healthcare");
     // chartGroup.append("text")
-    //     .attr("transform", "rotate(-90)")
-    //     .attr("y", 15 - margin.left)
-    //     .attr("x", 0 - (height / 2))
-    //     .attr("dy", "1em")
-    //     .classed("axis-text", true)
-    //     .text("Poverty(%)")
-    //     .attr("value","poverty")
-    //     .attr("id","test_bunny")
+        // .attr("transform", "rotate(-90)")
+        // .attr("y", 15 - margin.left)
+        // .attr("x", 0 - (height / 2))
+        // .attr("dy", "1em")
+        // .classed("axis-text", true)
+        // .text("Poverty(%)")
+        // .attr("value","poverty")
+        // .attr("id","test_bunny")
 
     // d3.select("#test_bunny")
     //     .on("click", function(){
