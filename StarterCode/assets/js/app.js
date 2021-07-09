1d72b62050420524917b0ceff8ecd183728027f9
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-07-09 14:12:54 -0700

    testing clickable text to change graph

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,37 +1,36 @@
 
+var svg_width = 1200;
+var svg_height = 600;
+
+var margin = {
+    top:50,
+    bottom:50,
+    left:80,
+    right:80
+}
+
+var height = svg_height - margin.bottom - margin.top;
+var width = svg_width - margin.left - margin.right;
+
+var chosen_x = "income"
+var chosen_y = "healthcare"
+
+function build_x_scale(census_data,chosen_x){
+    var x_scale = d3.scaleLinear()
+        .range([0,width])
+        .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
+    return(x_scale) 
+}
+
+var x_labels = ["income","obesity","age"]
+
 d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
     if (err) throw err;
-    var svg_width = 1200;
-    var svg_height = 600;
-
-    var margin = {
-        top:50,
-        bottom:50,
-        left:80,
-        right:80
-    }
-    
-    // var states = census_data.map(function(d){
-    //     return d.state
-    // })
-    // var income = census_data.map(function(d){
-    //     return d.income
-    // })
-    // var healthcare = census_data.map(function(d){
-    //     return +d.healthcare
-    // })
-    // var state = census_data.map(function(d){
-    //     return d.state
-    // })
-
-
     census_data.forEach(function(data){
         data.income = +data.income;
         data.healthcare = +data.healthcare
     })
     
-    var height = svg_height - margin.bottom - margin.top;
-    var width = svg_width - margin.left - margin.right;
 
     var svg = d3.select("#scatter_plot")
         .append("svg")
@@ -40,108 +39,107 @@ d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
 
     var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .attr("id","test")
+        .attr("id","test");
 
-    var chosen_x = "income"
-    var chosen_y = "healthcare"
 
-    var x_scale = d3.scaleLinear()
-        .range([0,width])
-        .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
-    var y_scale = d3.scaleLinear()
-        .range([height,0])
-        .domain([d3.min(census_data,d => d[chosen_y]*.7),d3.max(census_data,d => d[chosen_y])*1.2])
+    var xScale = build_x_scale(census_data,chosen_x)
+    var xAxis = d3.axisBottom(xScale)
 
-    // var y_scale = d3.scaleLinear()
-    //     .range([height,0])
-    //     .domain([d3.min(census_data[chosen_y]),d3.max(census_data[chosen_y])])
- 
-    // var yLinearScale = d3.scaleLinear()
-  //   .domain([0, d3.max(hairData, d => d.num_hits)])
-  //   .range([height, 0]);
+    var yScale = build_x_scale(census_data,chosen_y)
+    var yAxis = d3.axisBottom(yScale)
 
-    var bottom_axis = d3.axisBottom(x_scale)
-    // var left_axis = d3.axixLeft(y_scale)
-    
-
-    // var xscale = d3.scaleLinear()
-    //     .range([0,width])
-    //     .domain([d3.min(income)*0.8,d3.max(income)*1.2])  
     chartGroup.append("g")
         .attr("transform",`translate(0,${height})`)
-        .call(bottom_axis);
+        .call(xAxis)
+        .attr("id","x_axis")
 
-    // var yscale = d3.scaleLinear()
+
+// rough draft creating text labels
+    // var labels = chartGroup.append("text")
+    //     .attr("x", width/2.2)
+    //     .attr("y", height + margin.top)
+    //     .attr("value", "obesity") // value to grab for event listener// is this d.num_albums? ## 
+    //     .attr("id","obesity")
+    //     .text("Obesity");
+// rough draft making text clickable 
+    // d3.select("#obesity")
+    //     .on("click",function(){
+    //         var value = d3.select(this).attr("value")
+    //         chosen_x = value
+    //         var test_bunny1 = build_x_scale(census_data,chosen_x)
+    //         var test_bunny2 = d3.axisBottom(test_bunny1)
+    //         d3.select("#x_axis").transition().call(test_bunny2)
+    //     })
+
+
+
+
+    // var x_scale = d3.scaleLinear()
+    //     .range([0,width])
+    //     .domain([d3.min(census_data,d => d[chosen_x]*0.9),d3.max(census_data,d => d[chosen_x])*1.1])
+    // var y_scale = d3.scaleLinear()
     //     .range([height,0])
-    //     .domain([0,d3.max(healthcare)+2]);
-    
-    // var bottom_axis = d3.axisBottom(xscale)
-    
-    var left_axis = d3.axisLeft(y_scale)
+    //     .domain([d3.min(census_data,d => d[chosen_y]*.7),d3.max(census_data,d => d[chosen_y])*1.2])
 
-    // chartGroup.append("g")
+    // var bottom_axis = d3.axisBottom(x_scale)
+    
+
+    // var x_axis = chartGroup.append("g")
     //     .attr("transform",`translate(0,${height})`)
     //     .call(bottom_axis);
-    chartGroup.append("g")
-        .call(left_axis);
     
-    var circle_group = chartGroup.selectAll("circle")
-        .data(census_data)
-        .enter()
-        .append("g")
-        .attr("id","states")
-        .append("circle")
-        .attr("cx", d => x_scale(d.income))
-        .attr("cy",d => y_scale(d.healthcare))
-        .attr("r","20")
-        .attr("fill","steelblue")
-        .attr("opacity",".5")
-        .attr("stroke","steelblue")
+    // var left_axis = d3.axisLeft(y_scale)
 
+
+    // chartGroup.append("g")
+    //     .call(left_axis);
+    
     // var circle_group = chartGroup.selectAll("circle")
     //     .data(census_data)
     //     .enter()
     //     .append("g")
     //     .attr("id","states")
     //     .append("circle")
-    //     .attr("cx", d => xscale(d.income))
-    //     .attr("cy",d => yscale(d.healthcare))
+    //     .attr("cx", d => x_scale(d.income))
+    //     .attr("cy",d => y_scale(d.healthcare))
     //     .attr("r","20")
     //     .attr("fill","steelblue")
     //     .attr("opacity",".5")
     //     .attr("stroke","steelblue")
-    var test = d3.selectAll("#states")
-        .append("text")
-        .attr("x",d => x_scale(d.income))
-        .attr("y",d => y_scale(d.healthcare))
-        .text(d => d.abbr)
-        .style("fill","white")
-        .attr("dx", function(d){return -10})
-        .attr("dy", function(d){return 5})
 
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .classed("axis-text", true)
-        .text("Healthcare")
-        .attr("value","healthcare");
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 15 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .classed("axis-text", true)
-        .text("Poverty(%)")
-        .attr("value","poverty")
-        .attr("id","test_bunny")
 
-    d3.select("#test_bunny")
-        .on("click", function(){
-            var value = d3.select(this).attr("value")
-            chosen_x = value
-            console.log(chosen_x)
-        })
+    // var test = d3.selectAll("#states")
+    //     .append("text")
+    //     .attr("x",d => x_scale(d.income))
+    //     .attr("y",d => y_scale(d.healthcare))
+    //     .text(d => d.abbr)
+    //     .style("fill","white")
+    //     .attr("dx", function(d){return -10})
+    //     .attr("dy", function(d){return 5})
+
+    // chartGroup.append("text")
+    //     .attr("transform", "rotate(-90)")
+    //     .attr("y", 0 - margin.left)
+    //     .attr("x", 0 - (height / 2))
+    //     .attr("dy", "1em")
+    //     .classed("axis-text", true)
+    //     .text("Healthcare")
+    //     .attr("value","healthcare");
+    // chartGroup.append("text")
+    //     .attr("transform", "rotate(-90)")
+    //     .attr("y", 15 - margin.left)
+    //     .attr("x", 0 - (height / 2))
+    //     .attr("dy", "1em")
+    //     .classed("axis-text", true)
+    //     .text("Poverty(%)")
+    //     .attr("value","poverty")
+    //     .attr("id","test_bunny")
+
+    // d3.select("#test_bunny")
+    //     .on("click", function(){
+    //         var value = d3.select(this).attr("value")
+    //         chosen_x = value
+    //         console.log(chosen_x)
+    //     })
     });
     
