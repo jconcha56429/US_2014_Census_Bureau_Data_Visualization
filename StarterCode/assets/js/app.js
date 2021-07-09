14a30b243e40056bf75408920994215beff8dd2a
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-07-09 16:58:57 -0700

    figured figure out scaling of x/y axis and bubbles

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -61,6 +61,7 @@ d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
     var yAxis = d3.axisLeft(yScale)
     chartGroup.append("g")
         .call(yAxis)
+        .attr("id","y_axis")
 
     x_labels.forEach(function(label,i){
         chartGroup.append("text")
@@ -69,18 +70,19 @@ d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
         .attr("id", "x_label")
         .text(`${label} (%)`)
         .attr("value",`${label}`)
+        .attr("id","x")
     })
 
     y_labels.forEach(function(label,i){
         chartGroup.append("text")
             .attr("transform", "rotate(-90)")
-            .attr("y", 40 - margin.left - 15*i)
+            .attr("y", 40 - margin.left - 16*i)
             .attr("x", 10 - (height / 2))
             .attr("dy", "1em")
             .classed("axis-text", true)
             .text(`${label} (%)`)
             .attr("value",`${label}`)
-            .attr("id","test_bunny")
+            .attr("id","y")
     })
     var circle_group = chartGroup.selectAll("circle")
         .data(census_data)
@@ -96,16 +98,56 @@ d3.csv("../StarterCode/assets/data/data.csv").then(function(census_data,err){
         .attr("stroke","steelblue")
         .attr("id","states")
  
-    d3.selectAll("#states")
+    var state_labels = d3.selectAll("#states")
         .append("text")
-        .attr("x",d => xScale(d.income))
-        .attr("y",d => yScale(d.healthcare))
+        .attr("x",d => xScale(d[chosen_x]))
+        .attr("y",d => yScale(d[chosen_y]))
         .text(d => d.abbr)
         .style("fill","white")
         .attr("dx", function(d){return -10})
         .attr("dy", function(d){return 5})
 
+    d3.selectAll("#x").on("click",function(){
+        var value = d3.select(this).attr("value")
+        chosen_x = value
+        
+        var new_x_scale = build_x_scale(census_data,chosen_x)
+        var new_bottom_axis = d3.axisBottom(new_x_scale)
+        d3.select("#x_axis").transition().call(new_bottom_axis)
+        
+        circle_group.transition()
+        .duration(1000)
+        .attr("cx", d => new_x_scale(d[chosen_x]))
 
+        state_labels.transition()
+        .duration(1000)
+        .attr("x",d => new_x_scale(d[chosen_x]))
+    })
+    d3.selectAll("#y").on("click",function(){
+        var value = d3.select(this).attr("value")
+        chosen_y = value
+// investigate why this doesn't work but yScale() does \\ 
+        var new_y_scale = build_y_scale(census_data,chosen_y)
+        var new_left_axis = d3.axisLeft(new_y_scale)
+        d3.select("#y_axis").transition().call(new_left_axis)
+
+        circle_group.transition()
+        .duration(1000)
+        .attr("cy",d => yScale(d[chosen_y]))
+
+        state_labels.transition()
+        .duration(1000)
+        .attr("y",d => yScale(d[chosen_y]))
+
+    })
+        // 
+        // var new_y_scale = build_y_scale(census_data,chosen_y)
+        // var new_left_axis = d3.axisLeft(new_y_scale)
+        // d3.select("#y_axis").transition().call(new_left_axis)
+        // console.log(chosen_y)
+        // circle_group.transition()
+        // .duration(1000)
+        // .attr("cy",d => new_y_scale(d[chosen_y]))
 
 // rough draft creating text labels
     // var labels = chartGroup.append("text")
